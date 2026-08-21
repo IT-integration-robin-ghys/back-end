66fff816764731b83c1abf5904926a8d88bf52d0
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-22 01:55:52 +0200

    Update schema.sql to include the terrarium

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,12 @@ CREATE TABLE IF NOT EXISTS "user" (
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS "terrarium" (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    temperatures DOUBLE PRECISION [],
+    humidities DOUBLE PRECISION [],
+    user_id UUID,
+    FOREIGN KEY (user_id) REFERENCES "user"(id)
+);
