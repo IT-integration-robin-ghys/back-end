a5cd5acce4485da0d10222bd2c584a6151a22bf6
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-22 01:55:36 +0200

    Add terrariumrepo

---
+++ src/main/java/be/ucll/robinghys/integrationproject/terrarium/repository/TerrariumRepository.java
@@ -0,0 +1,10 @@
+package be.ucll.robinghys.integrationproject.terrarium.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import be.ucll.robinghys.integrationproject.terrarium.model.Terrarium;
+import be.ucll.robinghys.integrationproject.terrarium.model.TerrariumId;
+
+public interface TerrariumRepository extends JpaRepository<Terrarium, TerrariumId> {
+
+}
