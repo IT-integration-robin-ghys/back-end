1268205d8e1dbfc3536d85102eeb2b60df75dfcf
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-22 13:02:00 +0200

    Changed to auth service and added temporary endpoints to test the authorization

--- src/main/java/be/ucll/robinghys/integrationproject/user/controller/UserController.java
+++ src/main/java/be/ucll/robinghys/integrationproject/user/controller/UserController.java
@@ -1,13 +1,15 @@
 package be.ucll.robinghys.integrationproject.user.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import be.ucll.robinghys.integrationproject.user.dto.AuthenticationRequest;
 import be.ucll.robinghys.integrationproject.user.dto.AuthenticationResponse;
 import be.ucll.robinghys.integrationproject.user.dto.UserInput;
+import be.ucll.robinghys.integrationproject.user.service.AuthService;
 import be.ucll.robinghys.integrationproject.user.service.UserService;
 import jakarta.validation.Valid;
 
@@ -16,20 +18,33 @@ import jakarta.validation.Valid;
 public class UserController {
 
     private final UserService userService;
+    private final AuthService authService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthService authService) {
         this.userService = userService;
+        this.authService = authService;
     }
 
     @PostMapping("/login")
     public AuthenticationResponse login(@RequestBody AuthenticationRequest authenticationRequest) {
-        return userService.authenticate(authenticationRequest.email(), authenticationRequest.password());
+        return authService.loginUser(authenticationRequest.email(), authenticationRequest.password());
     }
 
     @PostMapping("/signup")
     public AuthenticationResponse signup(@Valid @RequestBody UserInput userDto) {
-        userService.signup(userDto);
-        return userService.authenticate(userDto.email(), userDto.password());
+        return authService.signup(userDto.username(), userDto.email(), userDto.password());
+    }
+
+    @PreAuthorize("hasRole('user')")
+    @GetMapping("/testJwtUser")
+    public String testJwtUser() {
+        return "success";
+    }
+
+    @PreAuthorize("hasRole('admin')")
+    @GetMapping("/testJwtAdmin")
+    public String testJwtAdmin() {
+        return "success";
     }
 
 }
