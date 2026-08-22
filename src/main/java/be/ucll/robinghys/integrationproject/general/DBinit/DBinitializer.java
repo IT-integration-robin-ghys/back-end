bf7c9850b1887874941961bf86aa73c918f360a1
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-22 13:04:33 +0200

    Refactored to work with new system

--- src/main/java/be/ucll/robinghys/integrationproject/general/DBinit/DBinitializer.java
+++ src/main/java/be/ucll/robinghys/integrationproject/general/DBinit/DBinitializer.java
@@ -1,11 +1,13 @@
 package be.ucll.robinghys.integrationproject.general.DBinit;
 
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
 import be.ucll.robinghys.integrationproject.terrarium.model.Terrarium;
 import be.ucll.robinghys.integrationproject.terrarium.repository.TerrariumRepository;
+import be.ucll.robinghys.integrationproject.user.model.Role;
 import be.ucll.robinghys.integrationproject.user.model.User;
 import be.ucll.robinghys.integrationproject.user.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
@@ -16,13 +18,13 @@ public class DBinitializer {
 
     private UserRepository userRepository;
     private TerrariumRepository terrariumRepository;
-    private PasswordEncoder passwordEncoder;
 
-    public DBinitializer(UserRepository userRepository, TerrariumRepository terrariumRepository,
-            PasswordEncoder passwordEncoder) {
+    public DBinitializer(
+            UserRepository userRepository,
+            TerrariumRepository terrariumRepository) {
+
         this.userRepository = userRepository;
         this.terrariumRepository = terrariumRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @PostConstruct
@@ -30,9 +32,13 @@ public class DBinitializer {
         terrariumRepository.deleteAll();
         userRepository.deleteAll();
 
-        String password = passwordEncoder.encode("password");
+        String password = BCrypt.withDefaults()
+                .hashToString(12, "password".toCharArray());
         User testUser = new User("Robin", "robin@email.com", password);
+        User testAdmin = new User("Admin", "admin@email.com", password);
+        testAdmin.setRole(Role.admin);
         userRepository.save(testUser);
+        userRepository.save(testAdmin);
 
         Terrarium terrarium = new Terrarium("terrarium 1");
         terrarium.addHumidities(1.0);
