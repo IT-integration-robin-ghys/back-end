4e90092c9a0be46906b04d9cc0e3188be999dba4
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-24 20:17:44 +0200

    Add terrariumRequestRepo to terrarium service and a create terrarium request service function

--- src/main/java/be/ucll/robinghys/integrationproject/terrarium/service/TerrariumService.java
+++ src/main/java/be/ucll/robinghys/integrationproject/terrarium/service/TerrariumService.java
@@ -1,22 +1,29 @@
 package be.ucll.robinghys.integrationproject.terrarium.service;
 
+import be.ucll.robinghys.integrationproject.terrarium.repository.TerrariumRequestRepository;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import be.ucll.robinghys.integrationproject.terrarium.dto.createTerrariumRequestDto;
 import be.ucll.robinghys.integrationproject.terrarium.dto.TerrariumsRequestDto;
 import be.ucll.robinghys.integrationproject.terrarium.model.Terrarium;
+import be.ucll.robinghys.integrationproject.terrarium.model.TerrariumRequest;
 import be.ucll.robinghys.integrationproject.terrarium.repository.TerrariumRepository;
 
 @Service
 @Validated
 public class TerrariumService {
 
+    private final TerrariumRequestRepository terrariumRequestRepository;
     private TerrariumRepository terrariumRepository;
 
-    public TerrariumService(TerrariumRepository terrariumRepository) {
+    public TerrariumService(TerrariumRepository terrariumRepository,
+            TerrariumRequestRepository terrariumRequestRepository) {
         this.terrariumRepository = terrariumRepository;
+        this.terrariumRequestRepository = terrariumRequestRepository;
     }
 
     public List<TerrariumsRequestDto> getTerrariumsByUserEmail(String email) {
@@ -28,4 +35,13 @@ public class TerrariumService {
                 .toList();
     }
 
+    public ResponseEntity<String> createTerrariumRequest(
+            createTerrariumRequestDto createTerrariumRequestDto) {
+        TerrariumRequest terrariumRequest = new TerrariumRequest(createTerrariumRequestDto.id(),
+                createTerrariumRequestDto.userId(),
+                createTerrariumRequestDto.terrariumId());
+        terrariumRequestRepository.save(terrariumRequest);
+
+        return ResponseEntity.ok("success");
+    }
 }
