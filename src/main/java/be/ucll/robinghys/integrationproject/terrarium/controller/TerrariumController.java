7ba19cb1e5c14e951970f870863352b0f96a44b5
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-24 22:30:25 +0200

    Add accept and deny endpoints for terrariumrequests

--- src/main/java/be/ucll/robinghys/integrationproject/terrarium/controller/TerrariumController.java
+++ src/main/java/be/ucll/robinghys/integrationproject/terrarium/controller/TerrariumController.java
@@ -1,17 +1,20 @@
 package be.ucll.robinghys.integrationproject.terrarium.controller;
 
 import be.ucll.robinghys.integrationproject.terrarium.dto.createTerrariumRequestDto;
+import be.ucll.robinghys.integrationproject.terrarium.model.TerrariumRequestId;
 import be.ucll.robinghys.integrationproject.terrarium.dto.GetTerrariumRequestDto;
 import be.ucll.robinghys.integrationproject.terrarium.dto.TerrariumsRequestDto;
 import be.ucll.robinghys.integrationproject.terrarium.service.TerrariumService;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,6 +47,20 @@ public class TerrariumController {
     public List<GetTerrariumRequestDto> getTerrariumRequestsByUserJWT() {
         return terrariumService
                 .getTerrariumRequestsByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+
+    @PostMapping("/link/{terrariumRequestId}/accept")
+    public ResponseEntity<String> acceptTerrariumRequestByTerrariumRequestId(@RequestParam UUID terrariumRequestId) {
+        return terrariumService.acceptTerrariumRequestByTerrariumRequestIdAndUserEmail(
+                new TerrariumRequestId(terrariumRequestId),
+                SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+
+    @PostMapping("/link/{terrariumRequestId}/deny")
+    public ResponseEntity<String> denyTerrariumRequestByTerrariumRequestId(@RequestParam UUID terrariumRequestId) {
+        return terrariumService.denyTerrariumRequestByTerrariumRequestIdAndUserEmail(
+                new TerrariumRequestId(terrariumRequestId),
+                SecurityContextHolder.getContext().getAuthentication().getName());
     }
 
 }
