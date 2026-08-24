1be94910445c8b82448b95993a21d9d8dae55a69
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-24 20:18:03 +0200

    Created endpoint using the createTerrariumRequest()

--- src/main/java/be/ucll/robinghys/integrationproject/terrarium/controller/TerrariumController.java
+++ src/main/java/be/ucll/robinghys/integrationproject/terrarium/controller/TerrariumController.java
@@ -1,15 +1,19 @@
 package be.ucll.robinghys.integrationproject.terrarium.controller;
 
+import be.ucll.robinghys.integrationproject.terrarium.dto.createTerrariumRequestDto;
 import be.ucll.robinghys.integrationproject.terrarium.dto.TerrariumsRequestDto;
 import be.ucll.robinghys.integrationproject.terrarium.service.TerrariumService;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/terrariums")
@@ -27,4 +31,11 @@ public class TerrariumController {
         return terrariumService
                 .getTerrariumsByUserEmail(SecurityContextHolder.getContext().getAuthentication().getName());
     }
+
+    @PostMapping("/link/create")
+    public ResponseEntity<String> createTerrariumToUser(
+            @RequestBody createTerrariumRequestDto createTerrariumRequestDto) {
+        return terrariumService.createTerrariumRequest(createTerrariumRequestDto);
+    }
+
 }
