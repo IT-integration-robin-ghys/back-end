5f33f6f7ab95a4d769612af7fe55d24ea3e5c8f0
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-24 17:39:12 +0200

    Had to add Embedded and AttributeOverride so it knows it shouldn't be THE id

--- src/main/java/be/ucll/robinghys/integrationproject/terrarium/model/TerrariumRequest.java
+++ src/main/java/be/ucll/robinghys/integrationproject/terrarium/model/TerrariumRequest.java
@@ -3,6 +3,9 @@ package be.ucll.robinghys.integrationproject.terrarium.model;
 import java.util.UUID;
 
 import be.ucll.robinghys.integrationproject.user.model.UserId;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -12,10 +15,15 @@ import jakarta.persistence.Table;
 public class TerrariumRequest {
 
     @EmbeddedId
+    @AttributeOverride(name = "id", column = @Column(name = "id"))
     private TerrariumRequestId id;
 
+    @Embedded
+    @AttributeOverride(name = "id", column = @Column(name = "user_id"))
     private UserId userId;
 
+    @Embedded
+    @AttributeOverride(name = "id", column = @Column(name = "terrarium_id"))
     private TerrariumId terrariumId;
 
     public TerrariumRequest(UUID id, UserId userId, TerrariumId terrariumId) {
