4db650552b0bcef2679e7a80384776e38041e53b
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-24 22:26:42 +0200

    FInd request by terrariumId

--- src/main/java/be/ucll/robinghys/integrationproject/terrarium/repository/TerrariumRequestRepository.java
+++ src/main/java/be/ucll/robinghys/integrationproject/terrarium/repository/TerrariumRequestRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import be.ucll.robinghys.integrationproject.terrarium.model.TerrariumId;
 import be.ucll.robinghys.integrationproject.terrarium.model.TerrariumRequest;
 import be.ucll.robinghys.integrationproject.terrarium.model.TerrariumRequestId;
 import be.ucll.robinghys.integrationproject.user.model.UserId;
@@ -11,5 +12,7 @@ import be.ucll.robinghys.integrationproject.user.model.UserId;
 public interface TerrariumRequestRepository extends JpaRepository<TerrariumRequest, TerrariumRequestId> {
 
     List<TerrariumRequest> findAllByUserId(UserId id);
+
+    TerrariumRequest findByTerrariumId(TerrariumId terrariumId);
 
 }
