a30d412d01467b72f670bcb17754581eb74fdbd1
RobinGhysUcll <robin.ghys@student.ucll.be>
2026-08-25 13:33:32 +0200

    Add the findAllByIdTerrariumId method so we can find the measurements connected to the terrarium

--- src/main/java/be/ucll/robinghys/integrationproject/sensorMeasurement/repository/SensorMeasurementRepository.java
+++ src/main/java/be/ucll/robinghys/integrationproject/sensorMeasurement/repository/SensorMeasurementRepository.java
@@ -1,10 +1,15 @@
 package be.ucll.robinghys.integrationproject.sensorMeasurement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import be.ucll.robinghys.integrationproject.sensorMeasurement.model.SensorMeasurement;
 import be.ucll.robinghys.integrationproject.sensorMeasurement.model.SensorMeasurementId;
+import be.ucll.robinghys.integrationproject.terrarium.model.TerrariumId;
 
 public interface SensorMeasurementRepository extends JpaRepository<SensorMeasurement, SensorMeasurementId> {
+
+    List<SensorMeasurement> findAllByIdTerrariumId(TerrariumId terrariumId);
 
 }
